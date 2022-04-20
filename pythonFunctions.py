c2db73d9b88e94e41e882d0fb45af1f15ddbcf88
unknown <rakdev03@iiht.tech>
2022-04-20 16:26:44 +0530

    functions

---
+++ pythonFunctions.py
@@ -0,0 +1,14 @@
+def myfunc():
+    print("inside myfunc")
+
+#with arguments
+def myfunc1(name):
+    print("my name is "+name)
+
+def fullName(fname, lname):
+    print(fname + " " + lname)
+
+
+myfunc()
+myfunc1("yash")
+fullName("yash","chandaliya")
